67482215c74ece3fe0b67f95fff0ad993c8f27fc
rahulyadav20111995 <rahulyadav574@gmail.com>
2016-07-23 13:11:18 +0530

    Utils for getting user location

--- app/src/main/java/com/example/brekkishhh/drivesmart/Activities/Landing.java
+++ app/src/main/java/com/example/brekkishhh/drivesmart/Activities/Landing.java
@@ -1,11 +1,13 @@
 package com.example.brekkishhh.drivesmart.Activities;
 
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.brekkishhh.drivesmart.R;
+import com.example.brekkishhh.drivesmart.Utils.Tracking;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -16,6 +18,7 @@ public class Landing extends AppCompatActivity implements OnMapReadyCallback{
 
 
     private MapFragment mainMap;
+    private Tracking tracking;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +28,8 @@ public class Landing extends AppCompatActivity implements OnMapReadyCallback{
         mainMap = MapFragment.newInstance();         //getting instance of the map object
         mainMap.getMapAsync(this);         //registering onMapReadyCallback
         this.addMapToLayout();                   //this method adds the map to the layout
+        tracking = new Tracking(Landing.this);
+
 
     }
 
@@ -37,6 +42,10 @@ public class Landing extends AppCompatActivity implements OnMapReadyCallback{
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        googleMap.addMarker(new MarkerOptions().position(new LatLng(28.1881,76.6115)).title("Hey Map Is Working"));
+       // googleMap.addMarker(new MarkerOptions().position(new LatLng(28.1881,76.6115)).title("Hey Map Is Working"));
+        LatLng userLatLng = tracking.fetchUserLocation();
+        googleMap.addMarker(new MarkerOptions().position(userLatLng).title("Hey I am Getting User's Position"));
+
+
     }
 }
---
+++ app/src/main/java/com/example/brekkishhh/drivesmart/Utils/Tracking.java
@@ -0,0 +1,105 @@
+package com.example.brekkishhh.drivesmart.Utils;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
+import android.nfc.Tag;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.brekkishhh.drivesmart.R;
+import com.google.android.gms.maps.model.LatLng;
+
+/**
+ * Created by Brekkishhh on 23-07-2016.
+ */
+public class Tracking {
+
+    private Context context;
+    private LocationManager locationManager;
+    private static final String TAG = "Tracking";
+
+    public Tracking(Context context) {
+        this.context =context;
+        this.locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+    }
+
+    public LatLng fetchUserLocation(){
+
+
+        Location networkLocation = null;
+        Location gpsLocation = null;
+
+        if (!isGpsAvailable() && !isNetworkAvailable()){
+            openLocationSettingsDialog().show();
+
+        }else {
+            if (isGpsAvailable()){
+                try {
+                    gpsLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    Log.d(TAG,gpsLocation.toString());
+
+                }catch (SecurityException ex){
+                    Log.e(TAG,"Error : "+ ex.getMessage());
+                }
+            }
+
+            if (isNetworkAvailable()){
+                try {
+                    networkLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                    Log.d(TAG,networkLocation.toString());
+
+                }catch (SecurityException ex){
+                    Log.e(TAG,"Error : "+ ex.getMessage());
+                }
+            }
+
+            if (gpsLocation==null && networkLocation!=null){
+                return new LatLng(networkLocation.getLatitude(),networkLocation.getLongitude());
+            }
+            else if (networkLocation == null && gpsLocation!=null){
+                return new LatLng(gpsLocation.getLatitude(),gpsLocation.getLongitude());
+            }
+            else {
+                return new LatLng(12.34,34.12);
+            }
+        }
+
+        Toast.makeText(context, "Unable To Get User Location", Toast.LENGTH_SHORT).show();
+        return new LatLng(0,0);
+
+    }
+
+    private boolean isGpsAvailable() {
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+    }
+
+    private boolean isNetworkAvailable() {
+        return locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+    }
+
+    private AlertDialog openLocationSettingsDialog(){
+
+        return new AlertDialog.Builder(context)
+                .setMessage(context.getString(R.string.gps_not_enabled))
+                .setTitle(context.getString(R.string.title_error))
+                .setPositiveButton("Enable GPS", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent settingsIntent = new Intent( Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        context.startActivity(settingsIntent);
+                    }
+                })
+                .create();
+
+
+    }
+
+
+
+}
