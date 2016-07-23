a9eab3e5f8667125c0ccd12bb985fc35424b3006
rahulyadav20111995 <rahulyadav574@gmail.com>
2016-07-23 11:37:02 +0530

    Map Fragment Added and unwanted dependencies removed

--- app/src/main/java/com/example/brekkishhh/drivesmart/Activities/Landing.java
+++ app/src/main/java/com/example/brekkishhh/drivesmart/Activities/Landing.java
@@ -1,19 +1,42 @@
 package com.example.brekkishhh.drivesmart.Activities;
 
 import android.os.Bundle;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.brekkishhh.drivesmart.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class Landing extends AppCompatActivity {
+public class Landing extends AppCompatActivity implements OnMapReadyCallback{
+
+
+    private MapFragment mainMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
 
+        mainMap = MapFragment.newInstance();         //getting instance of the map object
+        mainMap.getMapAsync(this);         //registering onMapReadyCallback
+        this.addMapToLayout();                   //this method adds the map to the layout
 
+    }
 
+    private void addMapToLayout(){
 
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.add(R.id.mapContainer,mainMap).commit();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(28.1881,76.6115)).title("Hey Map Is Working"));
     }
 }
